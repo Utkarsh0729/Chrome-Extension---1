4db4fd074eeaf8955486545d41e07d6ec8453880
Utkarsh0729 <162596961+Utkarsh0729@users.noreply.github.com>
2025-05-31 22:42:37 +0530

    Added bookmark button using content.js

--- Starter Files/content.js
+++ Starter Files/content.js
@@ -1,7 +1,20 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+   const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png");
+   
+   
+   window.addEventListener("load", addBookmarkButton);
+
+    function addBookmarkButton() {
+        const bookmarkButton = document.createElement("img");
+        bookmarkButton.id = "add-bookmark-button";
+        bookmarkButton.src = bookmarkImgURL;
+        bookmarkButton.style.height = "30px";
+        bookmarkButton.style.width = "30px";
+        bookmarkButton.style.margin = "0px 0px 8px";
+
+        const parentDiv = document.querySelector(".p-4");
+        parentDiv.insertBefore(bookmarkButton, parentDiv.children[1]);
+        
+        bookmarkButton.addEventListener('click', () => {
+        alert('Problem bookmarked!');
+        });
+    }

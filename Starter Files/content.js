64936a2e450c8a969c380c4fffec53f59a258ee7
Utkarsh0729 <162596961+Utkarsh0729@users.noreply.github.com>
2025-06-01 23:13:57 +0530

    "edited content.js"

--- Starter Files/content.js
+++ Starter Files/content.js
@@ -1,5 +1,5 @@
    const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png");
-   
+   const Problem_Key = "Problem_Key";
    
    window.addEventListener("load", addBookmarkButton);
 
@@ -14,7 +14,40 @@
         const parentDiv = document.querySelector(".p-4");
         parentDiv.insertBefore(bookmarkButton, parentDiv.children[1]);
         
-        bookmarkButton.addEventListener('click', () => {
-        alert('Problem bookmarked!');
+        bookmarkButton.addEventListener('click', addNewBookmarkHandler);
+    }
+    
+    async function addNewBookmarkHandler() {
+        const currentBookmarks = await getCurrentBookmarks();
+
+
+        const problemURL = window.location.href;
+        const ind = problemURL.indexOf("?", 26);
+        const uniqueId = problemURL.substring(26, ind);
+        const problemName = document.querySelector(".coding_problem_info_heading__G9ueL").textContent;
+
+        if(currentBookmarks.some((bookmark) => bookmark.id === uniqueId)) return;
+
+        const bookmarkObj = {
+            id: uniqueId,
+            name: problemName,
+            url: problemURL
+        }
+
+        const updatedBookmarks = [...currentBookmarks, bookmarkObj];
+
+        chrome.storage.sync.set({Problem_Key: updatedBookmarks}, () => {
+            alert('Problem bookmarked!');
+            console.log("Updated bookmarks to ", updatedBookmarks);
+        })
+
+    }
+
+
+    function getCurrentBookmarks() {
+        return new Promise((resolve, reject) => {
+            chrome.storage.sync.get([Problem_Key], (results) => {
+                resolve(results[Problem_Key] || []);
+            });
         });
     }
